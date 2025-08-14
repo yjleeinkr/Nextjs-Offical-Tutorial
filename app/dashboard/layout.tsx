de634639ca3fc0725e725039a65a3aba68f8f7d2
yjleeinkr <lucy.lee930216@gmail.com>
2025-08-14 09:47:31 +0900

    Partial PreRendering (PPR) 해보기 (experimental 기능)

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,19 @@
-import SideNav from "@/app/ui/dashboard/sidenav";
-import { PropsWithChildren } from "react";
+import SideNav from '@/app/ui/dashboard/sidenav';
+import type { PropsWithChildren } from 'react';
+
+/**
+ * 해당 경로 상에서 동적인 부분을 Suspense로 감싸고 있다면, Next가 해당 경로 중 어떤 부분이 정적이고, 동적인지 파악할 수 있다.
+ * 정적인 부분은 미리 렌더링해놓고, 사용자가 요청하기 전까지 동적인 부분은 렌더링을 미뤄둘 수 있다.
+ */
+// export const experimental_ppr = true; // PPR 가능해짐
 
 export default function Layout({ children }: PropsWithChildren) {
-  return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
-      </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-    </div>
-  );
+	return (
+		<div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+			<div className="w-full flex-none md:w-64">
+				<SideNav />
+			</div>
+			<div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+		</div>
+	);
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	// experimental: {
+	// 	ppr: 'incremental', // 특정 라우트들에서 PPR (Partial PreRendering) 가능해짐
+	// },
 };
 
 export default nextConfig;
