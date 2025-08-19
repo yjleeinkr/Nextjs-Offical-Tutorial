8351a0f32741b77a01a24197df17f7dd76fee0f1
yjleeinkr <lucy.lee930216@gmail.com>
2025-08-19 09:33:48 +0900

    쿼리를 사용한 search 기능 및 debounce 적용

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,41 @@
-export default function Page() {
-  return <p>Invoices Page</p>;
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+
+/** Page 컴포넌트는 searchParams라는 props를 허용하기 때문에 현재 URL params를 Table 컴포넌트에 넘겨줄 수 있다.
+ * - useSearchParams() 는 클라이언트 컴포넌트에서
+ * - searchParams는 Page 컴포넌트의 props로 받을 수 있기 때문에 서버 컴포넌트에서 사용 가능하다.
+ */
+export default async function Page(props: {
+	searchParams?: Promise<{
+		query?: string;
+		page?: string;
+	}>;
+}) {
+	console.log(await props.searchParams);
+	const searchParams = await props.searchParams;
+	const query = searchParams?.query || '';
+	const currentPage = Number(searchParams?.page) || 1;
+
+	return (
+		<div className="w-full">
+			<div className="flex w-full items-center justify-between">
+				<h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+			</div>
+			<div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+				<Search placeholder="Search invoices..." />
+				<CreateInvoice />
+			</div>
+			<Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+				<Table query={query} currentPage={currentPage} />
+			</Suspense>
+			<div className="mt-5 flex w-full justify-center">
+				{/* <Pagination totalPages={totalPages} /> */}
+			</div>
+		</div>
+	);
 }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,18 +1,43 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  return (
-    <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
-      <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
-      />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-    </div>
-  );
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
+	const { replace } = useRouter();
+
+	/** debounce 적용 */
+	const handleSearch = useDebouncedCallback((term: string) => {
+		const params = new URLSearchParams(searchParams);
+		if (term) {
+			params.set('query', term);
+		} else {
+			params.delete('query');
+		}
+		replace(`${pathname}?${params.toString()}`);
+	}, 300);
+
+	return (
+		<div className="relative flex flex-1 flex-shrink-0">
+			<label htmlFor="search" className="sr-only">
+				Search
+			</label>
+			<input
+				className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+				placeholder={placeholder}
+				onChange={(e) => handleSearch(e.target.value)}
+				defaultValue={searchParams.get('query')?.toString()}
+			/>
+			<MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+		</div>
+	);
 }
+/**
+ * defaultValue vs value / Controlled vs Uncontrolled
+ * 만약 state로 input의 입력값을 관리한다면 value 라는 속성을 사용해서 Controlled 컴포넌트로 만들어야 하는데 이 경우는 리액트가 input의 입력값을 관리하게 된다.
+ * 하지만 위의 케이스처럼 input의 입력값을 상태로 다룰게 아니라면 defaultValue라는 속성을 사용할 수 있다.
+ * state에 저장하는 대신 검색 쿼리를 url에 저장하고 있기 때문에 이 경우 브라우저의 기본 input 요소가 입력값의 상태를 스스로 관리하게 된다.
+ */
