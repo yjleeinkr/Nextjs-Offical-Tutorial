06c33fb2a8ece7c11eb50648bd982b6f761a17a8
yjleeinkr <lucy.lee930216@gmail.com>
2025-09-17 09:20:15 +0900

    form 접근성 개선 및 useActionState 사용 +  서버사이드 유효성 검사

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,24 +8,62 @@ import { redirect } from 'next/navigation';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
+/**
+ * safeParse(폼데이터객체).error.flatten().fieldErrors가 아래와 같이 나오기 때문에
+ * State.errors 타입을 이렇게 잡음
+ * {
+  	customerId: [ 'Please select a customer' ],
+  	amount: [ 'Please enter an amount greater than $0' ],
+  	status: [ 'Please select an invoice status' ]
+	}
+ * */
+export type State = {
+	errors?: {
+		customerId?: string[];
+		amount?: string[];
+		status?: string[];
+	};
+	message?: string | null;
+};
+
 const FormSchema = z.object({
 	id: z.string(),
-	customerId: z.string(),
-	amount: z.coerce.number(),
-	status: z.enum(['pending', 'paid']),
+	customerId: z.string({
+		invalid_type_error: 'Please select a customer',
+	}),
+	amount: z.coerce
+		.number()
+		.gt(0, { message: 'Please enter an amount greater than $0' }),
+	status: z.enum(['pending', 'paid'], {
+		invalid_type_error: 'Please select an invoice status',
+	}),
 	date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-	const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+	/** parse -> safeParse()
+	 * - success | error를 포함한 객체를 리턴해준다.
+	 * - 유효성 검증을 도와준다.
+	 * */
+	const validatedFields = CreateInvoice.safeParse({
 		customerId: formData.get('customerId'),
 		amount: formData.get('amount'),
 		status: formData.get('status'),
 		// 폼데이터 내 속성이 많다면, Object.fromEntries(formData) 사용
 	});
 
+	// DB 삽입 전 유효성 검사
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: 'Missing Fields. Failed to Create Invoice',
+		};
+	}
+
+	// 유효성 검사를 거친 데이터
+	const { customerId, amount, status } = validatedFields.data;
 	const amountInCents = amount * 100; // 자바스크립트 부동소수점 오류 제거를 위해 cents 단위로 전환
 	const date = new Date().toISOString().split('T')[0]; // YYYY-MM-DD 형식으로
 
@@ -35,7 +73,11 @@ export async function createInvoice(formData: FormData) {
 		VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
 	`;
 	} catch (err) {
+		// 데이터베이스 에러 발생 시
 		console.error(err);
+		return {
+			message: 'Database Error: Failed to Create Invoice.',
+		};
 	}
 
 	// DB 업데이트 후 /dashboard/invoices는 재검증되었고, 새로운 데이터가 페칭된다.
@@ -45,13 +87,27 @@ export async function createInvoice(formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-	const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+	id: string,
+	prevState: State,
+	formData: FormData,
+) {
+	const validatedFields = UpdateInvoice.safeParse({
 		customerId: formData.get('customerId'),
 		amount: formData.get('amount'),
 		status: formData.get('status'),
 	});
 
+	// DB 삽입 전 유효성 검사
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: 'Missing Fields. Failed to Update Invoice',
+		};
+	}
+
+	// 유효성 검사를 거친 데이터
+	const { customerId, amount, status } = validatedFields.data;
 	const amountInCents = amount * 100;
 	try {
 		await sql`
@@ -61,6 +117,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 	`;
 	} catch (err) {
 		console.error(err);
+		return { message: 'Database Error: Failed to Update Invoice.' };
 	}
 
 	revalidatePath('/dashboard/invoices');
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
@@ -7,7 +9,8 @@ import {
 	UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { createInvoice } from '@/app/lib/actions';
+import { createInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
 
 /** form action 속성
  * - 기존 HTML에선 form의 action속성에 해당 폼 데이터를 보낼 주소 URL(POST API 엔드포인트)를 입력했다. 즉, 사용자가 form을 제출하면 그 URL로 데이터가 전송됐다.
@@ -16,8 +19,36 @@ import { createInvoice } from '@/app/lib/actions';
  *   즉, 따로 API 라우트를 직접 작성하지 않아도 form의 action에 server action을 연결하면 바로 서버에 요청을 보낼 수 있다.
  */
 export default function Form({ customers }: { customers: CustomerField[] }) {
+	const initialState: State = { message: null, errors: {} };
+	/** useActionState(form action 속성에 붙인 서버 액션, 초기 상태)
+	 * 폼 상태와 폼 제출 시 호출되는 함수를 제공
+	 * - useActionState 첫번째 인자로 들어가는 폼 액션 함수의 인자
+	 * 	1. previousState: 기존 상태
+	 * 	2. formData: 폼 데이터
+	 * - useActionState 두번째 인자: 초기 상태값
+	 * - 배열 리턴
+	 * 1. state: 최신 폼 state
+	 * 2. formAction: 폼에서 사용할 새로운 액션함수
+	 * 3. isPending: 액션 대기 중 여부
+	 */
+	const [state, formAction] = useActionState(createInvoice, initialState);
+
+	/** 접근성을 위해 form 내부 요소들에 추가한 속성
+	 * 1. aria-describedby="customer-error"
+	 * - select 요소와 aria-describedby에 매겨진 customer-error를 id로 가진 요소 사이에 관계를 설정해준다.
+	 * - id="customer-error"인 요소가 select를 설명하는 요소라고 명시하는 것이다.
+	 * - 사용자가 select 태그에 접근 시 스크린 리더가 id="customer-error"인 요소 안의 메시지를 함께 읽어준다.
+	 * 2. aria-live="polite"
+	 * - aria-describedby에 연결된 요소의 내용이 바뀌면 사용자에게 어떤 모드로 알려줄지에 대한 속성이다.
+	 * - 내용이 바뀌면 스크린 리더가 자동으로 사용자에게 알려주는데, polite 모드는 사용자가 입력 중인 상황을 방해하지 않고 여유 있을 때 읽어준다.
+	 * - 반면 assertive를 쓰면 즉시 중단하고 곧바로 읽어주는데, 보통 오류 안내는 polite 모드를 쓰는게 적절하다고 한다.
+	 * 3. aria-atomic="true"
+	 * - 스크린 리더가 aria-live 영역 안의 컨텐츠 변경 시 어떤 부분을 읽어줄 지에 대한 속성이다.
+	 * - true일 경우, 전체 영역을 통째로 읽도록 강제한다. (맥락 놓칠 수 있을때 사용)
+	 * - false일 경우, 변경된 부분만 읽는다. (업데이트가 잦고, 전체를 매번 읽는다고 쳤을때 불필요하게 장황해지는 경우 사용)
+	 */
 	return (
-		<form action={createInvoice}>
+		<form action={formAction}>
 			<div className="rounded-md bg-gray-50 p-4 md:p-6">
 				{/* Customer Name */}
 				<div className="mb-4">
@@ -30,6 +61,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 							name="customerId"
 							className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
 							defaultValue=""
+							aria-describedby="customer-error"
 						>
 							<option value="" disabled>
 								Select a customer
@@ -41,6 +73,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 							))}
 						</select>
 						<UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+					</div>
+					<div id="customer-error" aria-live="polite" aria-atomic="true">
+						{state.errors?.customerId &&
+							state.errors.customerId.map((error) => (
+								<p className="mt-2 text-sm text-red-500" key={error}>
+									{error}
+								</p>
+							))}
 					</div>
 				</div>
 
@@ -58,9 +98,18 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 								step="0.01"
 								placeholder="Enter USD amount"
 								className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+								aria-describedby="amount-error"
 							/>
 							<CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
 						</div>
+					</div>
+					<div id="amount-error" aria-live="polite" aria-atomic="true">
+						{state.errors?.amount &&
+							state.errors.amount.map((error) => (
+								<p className="mt-2 text-sm text-red-500" key={error}>
+									{error}
+								</p>
+							))}
 					</div>
 				</div>
 
@@ -103,7 +152,20 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 							</div>
 						</div>
 					</div>
+					<div id="status-error" aria-live="polite" aria-atomic="true">
+						{state.errors?.status &&
+							state.errors.status.map((error) => (
+								<p className="mt-2 text-sm text-red-500" key={error}>
+									{error}
+								</p>
+							))}
+					</div>
 				</fieldset>
+				<div aria-live="polite" aria-atomic="true">
+					{state.message ? (
+						<p className="mt-2 text-sm text-red-500">{state.message}</p>
+					) : null}
+				</div>
 			</div>
 			<div className="mt-6 flex justify-end gap-4">
 				<Link
